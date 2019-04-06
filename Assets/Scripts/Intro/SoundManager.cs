61e192b679f4b9764dc1a9613a82b76df2fd1786
Dev7Kevin <dev7kevin.gamil.com>
2019-04-05 21:42:06 -0400

    Add Intro Scene BGM Fade in & out / NGUI / Resolution / Singlton / SceneManager

---
+++ Assets/Scripts/Intro/SoundManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public static SoundManager Instace { get; private set; }
+
+    private AudioSource m_AudioSource;
+
+	// Use this for initialization
+	void Start () {
+        if (Instace == null)
+            Instace = this;
+
+        m_AudioSource = GetComponent<AudioSource>();
+        m_AudioSource = gameObject.AddComponent<AudioSource>();        
+    }
+
+    public void FadeIn(AudioClip audioClip, float fadeTime)
+    {
+        if (m_AudioSource.clip != audioClip)
+        {
+            m_AudioSource.clip = audioClip;
+            m_AudioSource.volume = 0;
+            m_AudioSource.loop = true;
+            m_AudioSource.Play();
+        }
+
+        if (m_AudioSource.volume >= 1f)
+            return;
+
+
+        while (m_AudioSource.volume < 1)
+        {
+            m_AudioSource.volume += Mathf.Lerp(0f, 1f, Time.deltaTime / fadeTime);
+            return;
+        }
+
+        m_AudioSource.volume = 1f;
+    }
+
+    public void FadeOut(AudioClip audioClip, float fadeTime)
+    {
+        if(m_AudioSource.clip != audioClip)
+        {
+            m_AudioSource.clip = audioClip;
+        }
+
+        if (m_AudioSource.volume < 0f)
+            return;
+
+        while(m_AudioSource.volume > 0f)
+        {
+            m_AudioSource.volume -= Mathf.Lerp(0f, 1f, Time.deltaTime / fadeTime);
+            return;
+        }
+
+        m_AudioSource.volume = 0f;
+        if(m_AudioSource.isPlaying)
+        {
+            m_AudioSource.Stop();
+        }
+    }
+
+    public bool IsAudioPlaying()
+    {
+        return  m_AudioSource.isPlaying;
+    }
+}
---
+++ Assets/Scripts/Intro/Start_Game.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Game : MonoBehaviour {
+
+    private SoundManager m_SoundMgr;
+    private bool m_GoNextScene = false;
+
+    public UISlider m_UiSlider;
+
+    public AudioClip m_BMClip;
+    [Range(0f, 100f)] public float m_FadeTime;
+
+    private void Start()
+    {
+        m_SoundMgr = SoundManager.Instace;
+        if (!m_SoundMgr)
+            return;
+
+        if (!m_UiSlider)
+            return;
+    }
+
+    private void Update()
+    {
+        if (!m_GoNextScene)
+        {
+            m_SoundMgr.FadeIn(m_BMClip, m_FadeTime);
+            m_UiSlider.value = 0;
+        }
+           
+        else
+        {
+            m_SoundMgr.FadeOut(m_BMClip, m_FadeTime);
+            if(!m_SoundMgr.IsAudioPlaying())
+            {
+                StartGamePlay();
+            }
+
+            if (m_UiSlider.value < 1f)
+                m_UiSlider.value += Mathf.Lerp(0f, 1f, Time.deltaTime / m_FadeTime);
+        }
+    }
+
+    public void StartGamePlay()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void GoNextScene()
+    {
+        m_GoNextScene = true;
+    }
+}
