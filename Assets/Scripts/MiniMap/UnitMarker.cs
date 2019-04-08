2fd145777f71071c067310eabdbdddfbe2e46303
Dev7Kevin <dev7kevin.gamil.com>
2019-04-07 23:53:37 -0400

    MiniMap(Render Texture)

---
+++ Assets/Scripts/MiniMap/UnitMarker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMarker : MonoBehaviour {
+
+    public GameObject m_Unit;
+
+	// Use this for initialization
+	void Start () {
+        if (!m_Unit)
+            Debug.Log(m_Unit.name + "Null");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (m_Unit)
+        {
+            transform.position = new Vector3(m_Unit.transform.position.x, transform.transform.position.y, m_Unit.transform.position.z);
+            transform.forward = m_Unit.transform.forward;
+        }
+	}
+}
